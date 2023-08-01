fecaabadc4c66f8ffe1b0685034f4e9591e81797
FaelPressao <120026787+FaelPressao@users.noreply.github.com>
2023-08-01 13:27:47 -0300

    commit1

---
+++ academia/models/exercicio.py
@@ -0,0 +1,6 @@
+class exercicio():
+    def __init__ (self, nome, descricao, equipamento, tp_treino):
+        self.nome = nome
+        self.descricao = descricao
+        self.equipamento = equipamento
+        self.tp_treino = tp_treino
